587bc9e4cd9ad2616d58641521b671e13a01e76a
prasannapad <ppadmana@gmail.com>
2018-01-12 00:25:38 -0800

    Fix conflict in publicIP dns suffix name

--- FunctionsAppTest/FunctionsAppTest/Program.cs
+++ FunctionsAppTest/FunctionsAppTest/Program.cs
@@ -11,13 +11,27 @@ namespace FunctionsAppTest
         static void Main(string[] args)
         {
             var credentials = SdkContext.AzureCredentialsFactory
-    .FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+    .FromFile(@"..\..\azureauth.properties");
 
             var azure = Azure
                 .Configure()
                 .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
                 .Authenticate(credentials)
                 .WithDefaultSubscription();
+
+
+            var templatePath = @"https://raw.githubusercontent.com/prasannapad/ARMTemplatesPlay/master/FunctionsAppTest/FunctionsAppTest/CreateVMTemplate.json";
+            var paramPath = @"https://raw.githubusercontent.com/prasannapad/ARMTemplatesPlay/master/FunctionsAppTest/FunctionsAppTest/Parameters.json";
+
+
+            var deployment = azure.Deployments.Define("myDeployment")
+                .WithNewResourceGroup("PrasannaTestArm1", Region.USWest2)
+                .WithTemplateLink(templatePath, "1.0.0.0")
+                .WithParametersLink(paramPath, "1.0.0.0")
+                .WithMode(Microsoft.Azure.Management.ResourceManager.Fluent.Models.DeploymentMode.Incremental)
+                .Create();
+            Console.WriteLine("Press enter to delete the resource group...");
+            Console.ReadLine();
         }   
     }
 }
